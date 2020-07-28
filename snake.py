2fdd7edb843da6c0305da8abdcb1914852b38860
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-28 22:04:21 +0530

    first commit

---
+++ snake.py
@@ -0,0 +1,13 @@
+import pygame
+import time
+import random
+
+pygame.init()
+
+display_width=600
+display_height=400
+dis=pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('snake game')
+pygame.display.update()
+pygame.quit()
+quit()
