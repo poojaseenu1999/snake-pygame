36baf0d50bf31401547749bdc693027053c36988
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-28 22:15:24 +0530

    cretaing snake head

--- snake.py
+++ snake.py
@@ -3,11 +3,24 @@ import time
 import random
 
 pygame.init()
+#declare colors
+orangecolor=(255,123,7)
+blackcolor=(0,0,0)
+redcolor=(213,50,80)
+greencolor=(0,255,0)
+bluecolor=(50,153,213)
 
 display_width=600
 display_height=400
 dis=pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('snake game')
-pygame.display.update()
+snake_block=10
+game_over= False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+    pygame.draw.rect(dis,orangecolor,[200,150,snake_block,snake_block])
+    pygame.display.update()
 pygame.quit()
 quit()
